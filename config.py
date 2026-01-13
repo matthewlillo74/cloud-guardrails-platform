4e9028d030d4368a5f089ad55e1fa75ff58d83b5
Matthew <matthew74lillo@gmail.com>
2026-01-13 15:59:43 -0500

    test: trigger gitleaks with standard example key

---
+++ config.py
@@ -0,0 +1,4 @@
+# config.py
+# This is the "Standard Test Key" that all scanners are programmed to find
+aws_access_key = "AKIAIOSFODNN7EXAMPLE" 
+aws_secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
