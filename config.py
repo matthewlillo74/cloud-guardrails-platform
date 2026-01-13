b91f86b7bb4361ac46948da148404a7a8162954f
Matthew <matthew74lillo@gmail.com>
2026-01-13 16:11:41 -0500

    fix removed secrets

--- config.py
+++
@@ -1,5 +0,0 @@
-# config.py
-# This key matches the AWS pattern but isn't the standard "example" string
-# It should trigger the alarm.
-aws_access_key = "AKIATESTFAKEKEY12345"
-aws_secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
