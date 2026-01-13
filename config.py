4c9f8370ef76545f4c34f3d8df3dbd1ee5427cbf
Matthew <matthew74lillo@gmail.com>
2026-01-13 16:09:04 -0500

    trigger gitleaks

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 # config.py
-# This is the "Standard Test Key" that all scanners are programmed to find
-aws_access_key = "AKIAIOSFODNN7EXAMPLE" 
+# This key matches the AWS pattern but isn't the standard "example" string
+# It should trigger the alarm.
+aws_access_key = "AKIATESTFAKEKEY12345"
 aws_secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
